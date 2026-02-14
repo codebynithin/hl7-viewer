789e60d1eaa2433aa682d0e63f77bc8dff253607
Nithin V <nithinviswanathan@wimoku.com>
2026-02-14 12:02:32 +0530

    debugging

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/dist/",
   server: {
     host: true, // Expose to network
     port: 5173,
