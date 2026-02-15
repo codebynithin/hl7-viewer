0325aeb540809f210f7e8af305d62011d7c8b497
Nithin V <nithinviswanathan@wimoku.com>
2026-02-15 21:26:23 +0530

    feat: pwa changes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
-import { provideZoneChangeDetection } from '@angular/core';
+import { provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
-  providers: [provideZoneChangeDetection(), ...appConfig.providers],
+  providers: [
+    provideZoneChangeDetection(),
+    ...appConfig.providers,
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
 }).catch(err => console.error(err));
